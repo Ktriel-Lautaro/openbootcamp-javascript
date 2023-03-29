abbd12b2fb3fae50705d1465b816bd34c9e2f90d
catriel <catriellescobar@gmail.com>
2023-03-29 11:41:17 -0300

    segunda tarea

---
+++ misdatos2.js
@@ -0,0 +1,27 @@
+let fechaDeNacimiento = new Date('december 22 1995')
+let nombre="Catriel Escobar",
+edad=27,
+eresDesarrolador=true
+
+const libro = {
+    titulo:"los videos",
+    autor:nombre,
+    fecha:"22 de marzo de 1995",
+    url:"www.milibro.com"
+
+}
+
+
+
+const misDatos=[
+nombre,
+edad,
+fechaDeNacimiento,
+eresDesarrolador,
+libro,
+
+    
+
+]
+
+console.log(misDatos)
