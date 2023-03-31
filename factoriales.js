ebfa507ceb8b9d551deb8271b73f8b904bcccc2c
catriel <catriellescobar@gmail.com>
2023-03-31 09:40:43 -0300

    factoriales open bootcamp

---
+++ factoriales.js
@@ -0,0 +1,41 @@
+// FACTORIALES
+var a = 10
+
+for (let i = 10; i>=1; i--) {
+    
+    a = a*i
+
+    console.log(a)
+    
+}
+
+let i =10
+
+var b = 10
+
+while(i>=1){
+
+    b = b*i
+    i--
+    console.log(b)
+
+}
+
+
+multiplo = 10
+
+c= 10
+
+while (true) {
+
+ c = c*multiplo
+
+ multiplo--
+
+ if(multiplo=0){
+    break
+ }
+
+ console.log(c)
+    
+}
