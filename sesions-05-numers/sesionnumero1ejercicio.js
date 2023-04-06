ccfba73043651c72f566fe5559d29bf177d416e0
catriel <catriellescobar@gmail.com>
2023-04-06 12:40:40 -0300

    nueva tarea

---
+++ sesions-05-numers/sesionnumero1ejercicio.js
@@ -0,0 +1,26 @@
+altura = 179
+
+console.log(altura);
+
+alturaMetros= 1.79
+
+console.log(alturaMetros)
+
+peso= 85.6
+
+console.log(peso)
+
+alturaRedondeada= Math.ceil(alturaMetros)
+
+console.log(alturaRedondeada)
+
+pesoRedondeado= Math.floor(peso)
+
+console.log(pesoRedondeado)
+
+
+
+const numeroMaximo = Number.MAX_VALUE + 1;
+const respuestaNumeroMaximo = numeroMaximo === Number.MAX_VALUE;
+
+console.log(respuestaNumeroMaximo); // false
